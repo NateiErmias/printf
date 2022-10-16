df288144e66da5368577e75f439c6fae76112d44
NateiErmias <natei.ermias@aau.edu.et>
2022-10-16 03:18:37 -0700

    group task1

--- main.h
+++ main.h
@@ -29,7 +29,7 @@ int (*f)(va_list ap, flags_t *f);
 /* print_nums */
 int print_int(va_list l, flags_t *f);
 void print_number(int n);
-int printf_unsigned(va_list l, flags_t *f);
+int print_unsigned(va_list l, flags_t *f);
 int count_digit(int i);
 /* print_bases */
 int print_hex(va_list l, flags_t *f);
